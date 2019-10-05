f705505d77748a7d5337a4826e5fcb9bb9ed25e3
ziv <programmerziv@163.com>
2019-10-05 11:44:26 +0800

    task 换了个分支提交代码
    ===============================

--- src/main/java/socket/MyClient.java
+++ src/main/java/socket/MyClient.java
@@ -1,14 +1,17 @@
 package socket;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class MyClient {
     public static void main(String[] args) throws IOException {
         //绑定服务端的ip和端口
         Socket socket = new Socket("127.0.0.1",9999);
-
-
-
+        InputStream in = socket.getInputStream();
+        //接收数据
+        byte[] bs = new byte[1024];
+        in.read(bs);
+        System.out.println( new String(bs)+ " world");
     }
 }
